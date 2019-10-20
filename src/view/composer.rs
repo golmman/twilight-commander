882e5d541cf5634b737edb0faf5d8efda2a6d197
Dirk Kretschmann <d.kretschmann@dotsource.de>
2019-10-20 16:10:54 +0200

    exchanged unicode cross symbol

--- src/view/composer.rs
+++ src/view/composer.rs
@@ -44,7 +44,7 @@ impl Composer {
     }
 
     fn get_dir_prefix(&self, path_node: &PathNode) -> String {
-        let err_char = if self.config.composition.use_utf8 { '❌' } else { 'x' };
+        let err_char = if self.config.composition.use_utf8 { '⨯' } else { 'x' };
         let expanded_char = if self.config.composition.use_utf8 { '▼' } else { 'v' };
         let reduced_char = if self.config.composition.use_utf8 { '▶' } else { '>' };
 
