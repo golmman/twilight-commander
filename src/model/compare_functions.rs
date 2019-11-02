9469cdd1070cfe89958e91294c69ffe63c06a891
Dirk Kretschmann <d.kretschmann@dotsource.de>
2019-11-02 20:46:47 +0100

    added tests

--- src/model/compare_functions.rs
+++ src/model/compare_functions.rs
@@ -4,8 +4,17 @@ use std::cmp::Ordering;
 
 pub type PathNodeCompare = fn(&PathNode, &PathNode) -> Ordering;
 
-// TODO: add tests
 impl PathNode {
+    pub fn compare_dirs_bot_simple(a: &PathNode, b: &PathNode) -> Ordering {
+        if a.is_dir && !b.is_dir {
+            return std::cmp::Ordering::Greater;
+        } else if !a.is_dir && b.is_dir {
+            return std::cmp::Ordering::Less;
+        }
+
+        a.display_text.cmp(&b.display_text)
+    }
+
     pub fn compare_dirs_top_simple(a: &PathNode, b: &PathNode) -> Ordering {
         if a.is_dir && !b.is_dir {
             return std::cmp::Ordering::Less;
@@ -14,10 +23,6 @@ impl PathNode {
         }
 
         a.display_text.cmp(&b.display_text)
-    }
-
-    pub fn compare_dirs_bot_simple(a: &PathNode, b: &PathNode) -> Ordering {
-        Self::compare_dirs_top_simple(b, a)
     }
 
     pub fn get_path_node_compare(config: &Config) -> PathNodeCompare {
@@ -30,5 +35,94 @@ impl PathNode {
             };
 
         path_node_compare
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cmp::Ordering::Greater;
+    use std::cmp::Ordering::Less;
+
+    mod compare_dirs_bot_simple_tests {
+        use super::*;
+
+        #[test]
+        fn dir_to_dir() {
+            let dir_a = get_dir("dir_a");
+            let dir_b = get_dir("dir_b");
+
+            let order = PathNode::compare_dirs_bot_simple(&dir_a, &dir_b);
+
+            assert_eq!(Less, order);
+        }
+
+        #[test]
+        fn dir_to_file() {
+            let dir = get_dir("something");
+            let file = get_file("something");
+
+            let order = PathNode::compare_dirs_bot_simple(&dir, &file);
+
+            assert_eq!(Greater, order);
+        }
+
+        #[test]
+        fn file_to_file() {
+            let file_a = get_file("file_a");
+            let file_b = get_file("file_b");
+
+            let order = PathNode::compare_dirs_bot_simple(&file_a, &file_b);
+
+            assert_eq!(Less, order);
+        }
+    }
+
+    mod compare_dirs_top_simple_tests {
+        use super::*;
+
+        #[test]
+        fn dir_to_dir() {
+            let dir_a = get_dir("dir_a");
+            let dir_b = get_dir("dir_b");
+
+            let order = PathNode::compare_dirs_top_simple(&dir_a, &dir_b);
+
+            assert_eq!(Less, order);
+        }
+
+        #[test]
+        fn dir_to_file() {
+            let dir = get_dir("something");
+            let file = get_file("something");
+
+            let order = PathNode::compare_dirs_top_simple(&dir, &file);
+
+            assert_eq!(Less, order);
+        }
+
+        #[test]
+        fn file_to_file() {
+            let file_a = get_file("file_a");
+            let file_b = get_file("file_b");
+
+            let order = PathNode::compare_dirs_top_simple(&file_a, &file_b);
+
+            assert_eq!(Less, order);
+        }
+    }
+
+    fn get_dir(name: &str) -> PathNode {
+        let mut path_node = PathNode::from(".");
+        path_node.is_dir = true;
+        path_node.display_text = String::from(name);
+        path_node
+    }
+
+    fn get_file(name: &str) -> PathNode {
+        let mut path_node = PathNode::from(".");
+        path_node.is_dir = false;
+        path_node.display_text = String::from(name);
+        path_node
     }
 }
