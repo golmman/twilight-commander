9473f9ea6d330419804e75945e74d43500afa00f
Dirk Kretschmann <kretschmanndi@gmail.com>
2020-12-13 11:51:44 +0100

    added tcide_neovim and made it the default option via tcide symlink

--- dev/install.sh
+++ dev/install.sh
@@ -6,6 +6,8 @@ if [ "$EUID" -ne 0 ]
 fi
 
 cp target/release/twilight-commander /usr/local/bin/twilight-commander
+cp tcide_vim /usr/local/bin/tcide_vim
+cp tcide_neovim /usr/local/bin/tcide_neovim
 cp tcide /usr/local/bin/tcide
 
 echo "twilight-commander was installed/updated"
